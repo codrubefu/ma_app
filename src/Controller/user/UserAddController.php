3fd0489937afb0ba9d049cd338bf86d111f283a8
Codrut Befu <cb@sologics.de>
2019-10-07 15:33:40 +0300

    create login and security

--- src/Controller/user/UserAddController.php
+++ src/Controller/user/UserAddController.php
@@ -7,6 +7,7 @@ namespace App\Controller\user;
 use App\Entity\Users;
 use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,8 @@ class UserAddController extends AbstractController
 {
 
     /**
+     *
+     * @IsGranted("ROLE_ADMIN")
      * @Route("user/add")
      */
 
@@ -32,5 +35,14 @@ class UserAddController extends AbstractController
         return $this->render('user/add.html.twig',[
             'userForm' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/Api/account",name="api_account")
+     */
+
+    public function accountApi(){
+        $user = $this->getUser();
+        return $this->json($user);
     }
 }
