3ccacef1fd7d21e8ff5e8d27f3ec0578fa29a9ac
Codrut Befu <cb@sologics.de>
2019-10-07 17:16:06 +0300

    joaca cu vujs

---
+++ public/vuejs/src/layout/dashboard/ContentSubfooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid d-flex flex-wrap justify-content-between ">
+      <nav>
+        This is my super footer
+      </nav>
+    </div>
+  </footer>
+</template>
+<script>
+    export default {};
+</script>
+<style>
+</style>
